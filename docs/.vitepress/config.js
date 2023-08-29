2b524c26068580640910d2a808c3f82862c9a8f4
qbclub <17926057+qbclub@users.noreply.github.com>
2023-08-29 15:23:09 +0400

    redesign

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,43 +2,49 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
 
-  title: 'Города и Веси',
-  description: 'Крутой инструмент',
+  title: 'Города и веси',
+  description: 'Информация проекта Гророда и веси',
   lang: 'ru-RU',
+
   themeConfig: {
+    aside: false,
+    logo: '/logo.png',
     nav: [
       { text: 'Документы', link: '/privacy/main' },
       { text: 'Обучение', link: '/education/main' },
-      { text: 'О проекте', link: '/about/main' },
+      { text: 'О проекте', link: '/' },
     ],
     sidebar: {
       '/privacy/': [
         {
           text: 'Документы',
+          link: '/privacy/main.md',
           items: [
-            
+
             { text: 'Оферта', link: '/privacy/oferta.md' },
             { text: 'Агентский договор', link: '/privacy/agentDocs.md' },
             { text: 'Согласие на обработку персональных данных', link: '/privacy/agreement.md' }
           ]
         }
       ],
-      '/about/': [
+      '/': [
         {
           text: 'О проекте',
+          link: '/',
           items: [
-            
-            { text: 'Вопросы', link: '/about/questions.md' },
-            { text: 'Поддержка', link: '/about/support.md' },
-            { text: 'Контакты', link: '/about/contacts.md' }
+
+            { text: 'Вопросы', link: '/questions.md' },
+            { text: 'Поддержка', link: '/support.md' },
+            { text: 'Контакты', link: '/contacts.md' }
           ]
         }
       ],
       '/education/': [
         {
           text: 'Обучение',
+          link:"/education/main.md",
           items: [
-            
+
             { text: 'Создать поездку', link: '/education/create-trip.md' },
             { text: 'Регистрация', link: '/education/reg.md' },
           ]
